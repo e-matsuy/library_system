e62a8ade708179ba027b0d94a4ee10233fb4b428
e-matsuy <matsuda.yu@e-jan.co.jp>
2020-07-14 18:57:26 +0900

    add date parse format

--- src/main/java/LibraryManager/util/GsonSingleton.java
+++ src/main/java/LibraryManager/util/GsonSingleton.java
@@ -6,7 +6,11 @@ import com.google.gson.GsonBuilder;
 import java.io.Reader;
 
 public final class GsonSingleton {
-    private static final Gson GSON_INSTANCE = new GsonBuilder().setLenient().serializeNulls().create();
+    private static final Gson GSON_INSTANCE = new GsonBuilder()
+                                                    .setLenient()
+                                                    .serializeNulls()
+                                                    .setDateFormat("yyyy/MM/dd")
+                                                    .create();
 
     public static synchronized <T> T fromJson(String jsonString, Class<T> classoft){
         return GSON_INSTANCE.fromJson(jsonString, classoft);
